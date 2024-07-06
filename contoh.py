5e21aef458e79a68ba91ebc2491d22a0fcb8fa7f
Jeminana <sadenajmi@gmail.com>
2024-07-06 21:02:59 +0800

    Update example

--- contoh.py
+++ contoh.py
@@ -1,1 +1,12 @@
-print("what up")
+class Murid:
+    def __init__(self, nama, kelas, nilai):
+        self.nama = nama
+        self.kelas = kelas
+        self.nilai = nilai
+    
+    def cetak(self):
+        print("Nama : ", self.nama)
+        print("Kelas : ", self.kelas)
+        print("Nilai : ", self.nilai)
+    
+    
